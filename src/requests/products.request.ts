ab5914accd7272ed1c0348baa1bc26105989e88f
Brenno Luan <brennoluan8530@gmail.com>
2026-05-29 19:43:27 -0300

    feat: add createProduct function for API product creation

--- src/requests/products.request.ts
+++ src/requests/products.request.ts
@@ -10,3 +10,18 @@ export async function fetchProductById(id: string | number): Promise<Product> {
   const { data } = await api.get<Product>(`/products/${id}`);
   return data;
 }
+
+export async function createProduct(product: Product): Promise<Product> {
+  const { data } = await api.post<Product>("/products", product);
+  return data;
+}
+
+// const newProduct: Product = {
+//   id: Date.now(),
+//   label: `Camiseta ${Date.now()}`,
+//   price: 28,
+//   colors: ["Bege", "Branca", "Cinza"],
+//   imageSrc:
+//     "https://raw.githubusercontent.com/gss-patricia/use-dev-assets/refs/heads/main/cards-produtos/cards-home/desktop-e-tablet/capy.png",
+//   description: "Camiseta 100% algodão.",
+// };
